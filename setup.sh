df1601a697440e222d65d4e26676dc375cb77a67
venkata <srampv@gmail.com>
2023-01-06 10:03:55 -0600

    Create setup.sh
    this is to copy files to corresponding folders

---
+++ setup.sh
@@ -0,0 +1,8 @@
+  mkdir -p device/samsung/gtaxlwifi
+     mkdir -p vendor/samsung/gtaxlwifi
+     mkdir -p hardware/samsung/gtaxlwifi
+     mkdir -p kernel/samsung/exynos7870
+     cp -r ~/Downloads/gtaxlwifi_device_tree-main/* device/samsung/gtaxlwifi/
+     cp -r ~/Downloads/gtaxlwifi_vendor_tree-main/* vendor/samsung/gtaxlwifi/
+     cp -r ~/Downloads/gtaxl_wifi_hardware-main/* hardware/samsung/gtaxlwifi/
+    cp -r ~/Downloads/gtaxl_kernel_samsung_exynos7870-main/*  kernel/samsung/exynos7870/
